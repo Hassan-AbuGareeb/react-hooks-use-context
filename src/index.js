2e6318016944a272e93fa45aad649bc585ec3f2b
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-11-21 21:47:33 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { UserProvider } from "./context/user";
+import { ThemeProvider } from "./context/theme";
+ReactDOM.render(
+  <ThemeProvider>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </ThemeProvider>,
+  document.getElementById("root")
+);
